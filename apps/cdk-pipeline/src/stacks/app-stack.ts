13f2482805c1300be20664aa50772055dc8bd6e7
nwitte-rocketloans <normwitte@rocketloans.com>
2021-10-22 19:15:08 +0000

    Update to new CodePipelines for CDK

--- apps/cdk-pipeline/src/stacks/app-stack.ts
+++ apps/cdk-pipeline/src/stacks/app-stack.ts
@@ -1,8 +1,10 @@
 import { Stack, Construct, StackProps, SecretValue } from '@aws-cdk/core';
-import { Artifact } from '@aws-cdk/aws-codepipeline';
-import { GitHubSourceAction } from '@aws-cdk/aws-codepipeline-actions';
-import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
-import { LinuxBuildImage, ComputeType } from '@aws-cdk/aws-codebuild';
+import {
+  CodePipeline,
+  ShellStep,
+  CodePipelineSource,
+} from '@aws-cdk/pipelines';
+import path = require('path');
 import { AppStage } from './app-stage';
 import { env } from './global-config';
 
@@ -10,47 +12,36 @@ export class AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const sourceArtifact = new Artifact();
-    const cloudAssemblyArtifact = new Artifact();
+    const authentication = SecretValue.secretsManager('GITHUB_SECRET_PAT');
 
-    const githubSourceAction = new GitHubSourceAction({
-      actionName: 'GitHub',
-      output: sourceArtifact,
-      oauthToken: SecretValue.secretsManager('GITHUB_SECRET_PAT'),
-      owner: 'nwitte-rocketloans',
-      repo: 'nx-cdk-demo',
-      branch: 'main',
+    const input = CodePipelineSource.gitHub(
+      'nwitte-rocketloans/nx-cdk-demo',
+      'feature/upgrade-cdk',
+      { authentication }
+    );
+
+    const synth = new ShellStep('Synth', {
+      input,
+      commands: [
+        'yarn install --frozen-lockfile',
+        'yarn nx run cdk-pipeline:build',
+        'yarn nx run cdk-pipeline:synth',
+      ],
+      primaryOutputDirectory: path.join(
+        __dirname,
+        '../../../..',
+        'dist/apps/cdk-pipeline/cdk.out'
+      ),
     });
 
-    const synthAction = SimpleSynthAction.standardYarnSynth({
-      sourceArtifact,
-      cloudAssemblyArtifact,
-      subdirectory: 'apps/cdk-pipeline',
-      installCommand: 'yarn install --frozen-lockfile',
-      buildCommand: 'yarn nx run cdk-pipeline:build',
-      synthCommand: 'yarn nx run cdk-pipeline:synth',
-      environmentVariables: {
-        SOURCE_ACTION_COMMIT_ID: {
-          value: githubSourceAction.variables.commitId,
-        },
-      },
-      environment: {
-        buildImage: LinuxBuildImage.AMAZON_LINUX_2_3,
-        computeType: ComputeType.LARGE,
-        privileged: true,
-      },
-    });
-
-    const pipeline = new CdkPipeline(this, 'Pipeline', {
-      pipelineName: `nx-cdk-demo-pipeline`,
-      cloudAssemblyArtifact,
-      sourceAction: githubSourceAction,
-      synthAction,
-      selfMutating: true,
+    const pipeline = new CodePipeline(this, 'CodePipeline', {
+      synth,
+      selfMutation: true,
+      pipelineName: 'nx-cdk-demo-pipeline',
     });
 
     const stage = new AppStage(this, 'AppStage', { env });
 
-    pipeline.addApplicationStage(stage);
+    pipeline.addStage(stage);
   }
 }
